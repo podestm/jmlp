772a872984a9789707708b95e15ac3324ed660a1
Michal Podešť <121830981+podestm@users.noreply.github.com>
2024-10-05 19:20:55 +0200

    adding post management functions v1

--- admin_views.py
+++ admin_views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
-from flask_login import login_required
+from flask_login import login_required, current_user
+from models import Note
 
 admin_bp = Blueprint('admin', __name__)
 
@@ -9,13 +10,32 @@ admin_bp = Blueprint('admin', __name__)
 def home():
     return render_template('admin/home.html')
 
+
 @admin_bp.route('/dashboard')
 @login_required
 def dashboard():
     return render_template('admin/dashboard.html')
 
+
 @admin_bp.route('/settings')
 @login_required
 def settings():
     return render_template('admin/settings.html')
+
+
+## Post management pages
+@admin_bp.route('/posts', methods=['GET', 'POST'])
+@login_required
+def posts():
+    posts = Note.query.all()
+    blog_posts = [{'data': post,
+                   'note_title': post.name,
+                   'note_id': post.id,
+                   'note_data': post.data,
+                   'note_date': post.date,
+                   } 
+            for post in posts
+    ]
+    
+    return render_template('admin/posts.html', user=current_user, blog_posts=blog_posts)
 
