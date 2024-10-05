9021478bb0895612cfe9ad215fba59a2fb2ae111
Michal Podešť <121830981+podestm@users.noreply.github.com>
2024-10-05 20:51:50 +0200

    v2.1.4. adding post management functions

--- admin_views.py
+++ admin_views.py
@@ -22,7 +22,10 @@ def settings():
                    'note_id': post.id,
                    'note_data': post.data,
                    'note_date': post.date,
-                   } 
+                   'note_type': post.Note_type,
+                   'note_image': post.Note_image_url != None,
+                   'image_url': post.Note_image_url
+                   }
             for post in posts
     ]
     
