2f2cc521c8c9c9252cfa8007c5f45d90fb913226
Michal Podešť <121830981+podestm@users.noreply.github.com>
2024-10-05 20:29:55 +0200

    v2.1.4. adding post management functions

--- admin_views.py
+++ admin_views.py
@@ -13,16 +13,22 @@ def home():
     return render_template('admin/home.html')
 
 
-@admin_bp.route('/dashboard')
-@login_required
-def dashboard():
-    return render_template('admin/dashboard.html')
-
-
-@admin_bp.route('/settings')
+@admin_bp.route('/settings', methods=['GET', 'POST'])
 @login_required
 def settings():
-    return render_template('admin/settings.html')
+    post_details = None
+    if request.method == 'POST': 
+        note = request.form.get('post')
+        title = request.form.get('post_title')
+
+        if len(note) < 1:
+            flash('Note is too short!', category='error') 
+        else:
+            new_note = Note(data=note, user_id=current_user.id, name=title) 
+            db.session.add(new_note)
+            db.session.commit()
+            return redirect(url_for('admin.posts'))
+    return render_template('admin/settings.html', user=current_user, post_details=post_details)
 
 
 ## Post management pages
