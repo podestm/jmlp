4461d320179c016a64dbb78bfe3577e8f20899ba
Michal Podešť <121830981+podestm@users.noreply.github.com>
2024-10-05 22:57:50 +0200

    v2.2. adding post management functions

--- admin_views.py
+++ admin_views.py
@@ -1,6 +1,7 @@
 import os
 from config import db 
 from flask import Blueprint, render_template, request, redirect, url_for, flash
+from sqlalchemy import inspect, update
 from flask_login import login_required, current_user
 from models import Notes, Event
 
@@ -18,8 +19,9 @@ def home():
 def settings():
         return render_template('admin/settings.html')
 
+### POSTS MANAGEMENT ###
 
-
+#Posts overview page
 @admin_bp.route('/posts', methods=['GET', 'POST'])
 @login_required
 def posts():
@@ -39,5 +41,46 @@ def posts():
 @admin_bp.route('/post-add', methods=['GET', 'POST'])
 @login_required
 def add_post():
-    return render_template('admin/post_add.html')
+    post_details = None
+    if request.method == 'POST': 
+        note = request.form.get('post')
+        title = request.form.get('post_title')
 
+        if len(note) < 1:
+            flash('Note is too short!', category='error') 
+        else:
+            new_note = Notes(data=note, user_id=current_user.id, name=title) 
+            db.session.add(new_note)
+            db.session.commit()
+            return redirect(url_for('admin.posts'))
+    return render_template('admin/post_add.html', user=current_user, post_details=post_details)
+
+
+# Post editing page - basically same template as for adding but prefilled and used update function from SQL Alchemy 
+@admin_bp.route('/post-edit/<int:post_id>', methods=['GET', 'POST'])
+@login_required
+def edit_post(post_id):
+    post = Notes.query.filter(Notes.id == post_id).first_or_404()
+    post_details = {
+        'id': post.id,
+        'name': post.name,
+        'date': post.date,
+        'data': post.data,
+        }
+    
+    if request.method == 'POST': 
+        post_data = request.form.get('post')
+        post_title = request.form.get('post_title')
+
+        if len(post_data) < 1:
+            flash('Note is too short!', category='error') 
+        else:
+            update_post = (
+                update(Notes).where(Notes.id == post_id)
+                .values(name = post_title, data = post_data)
+            )
+            db.session.execute(update_post)
+            db.session.commit()
+            
+            return redirect(url_for('admin.posts'))
+    return render_template('admin/post_add.html', user=current_user, post_details=post_details)
