af10cbef7cd96256a0ab6b665368204f3493d58e
Michal Podešť <121830981+podestm@users.noreply.github.com>
2024-10-05 19:46:16 +0200

    adding post management functions v2.1.2.

--- admin_views.py
+++ admin_views.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
-from flask_login import login_required
+import os
+from config import db 
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask_login import login_required, current_user
 from models import Note
 
 admin_bp = Blueprint('admin', __name__)
@@ -28,4 +30,23 @@ def settings():
 @login_required
 def posts():
     return render_template('admin/posts.html')
+
+
+# Post adding page
+@admin_bp.route('/post-add', methods=['GET', 'POST'])
+@login_required
+def add_post():
+    post_details = None
+    if request.method == 'POST': 
+        note = request.form.get('post')
+        title = request.form.get('post_title')
+
+        if len(note) < 1:
+            flash('Note is too short!', category='error') 
+        else:
+            new_note = Note(data=note, user_id=current_user.id, name=title) 
+            db.session.add(new_note)
+            db.session.commit()
+            return redirect(url_for('admin.posts'))
+    return render_template('admin/post_add.html', user=current_user, post_details=post_details)
 
